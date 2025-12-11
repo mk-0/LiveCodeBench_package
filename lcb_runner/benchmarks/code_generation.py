4a2c2785cb3942a93295e69feba13825dc2056c5
mk <mkitov09@gmail.com>
2025-12-11 14:53:37 +0100

    Use Parquet dataset

--- lcb_runner/benchmarks/code_generation.py
+++ lcb_runner/benchmarks/code_generation.py
@@ -120,9 +120,8 @@ class CodeGenerationProblem:
             ),
         }
 
-
-def load_code_generation_dataset(release_version="release_v1", start_date=None, end_date=None) -> list[CodeGenerationProblem]:
-    dataset = load_dataset("livecodebench/code_generation_lite", split="test", version_tag=release_version, trust_remote_code=True)
+def load_code_generation_dataset(release_version="release_v1", start_date=None, end_date=None, revision="refs/pr/7") -> list[CodeGenerationProblem]:
+    dataset = load_dataset("livecodebench/code_generation_lite", release_version, split="test", revision=revision)
     dataset = [CodeGenerationProblem(**p) for p in dataset]  # type: ignore
     if start_date is not None:
         p_start_date = datetime.strptime(start_date, "%Y-%m-%d")
